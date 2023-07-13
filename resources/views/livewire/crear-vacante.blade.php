f2dc6bed833e03108d57dbc7177cf0d2246622a6
Paolo M <paolom22@gmail.com>
2023-07-13 08:45:04 -0400

    se crea dashboard principal

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,8 +1,19 @@
 <form class="md:w-1/2">
-    <!-- Email Address -->
+    <!-- Titulo -->
     <div class="mt-4">
-        <x-input-label for="title" :value="__('Titulo')" />
-        <x-text-input id="title" class="block mt-2 w-full" type="text" name="title" :value="old('title')" placeholder='Titulo de la Vacante' required />
-        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+        <x-input-label for="titulo" :value="__('Titulo')" />
+        <x-text-input id="titulo" class="block mt-2 w-full" type="text" name="titulo" :value="old('titulo')" placeholder='Titulo de la Vacante' required />
+        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
+    </div>
+    <!-- Salario -->
+    <div class="mt-4">
+        <x-input-label for="salario" :value="__('Salario')" />
+        <select 
+            id="salario"
+            name="salario"
+            class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+
+        ></select>
+        <x-input-error :messages="$errors->get('salario')" class="mt-2" />
     </div>
 </form>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VacantesController;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', [DashboardController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/vacantes', [VacantesController::class,'index'])->middleware(['auth', 'verified'])->name('vacantes.index');
 Route::get('/vacantes/crear', [VacantesController::class,'create'])->middleware(['auth', 'verified'])->name('vacantes.create');
